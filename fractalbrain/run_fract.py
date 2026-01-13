f8c71e3ffc7215953baa048ccacc3c92559e9bdd
rohan3412 <114685156+rohan3412@users.noreply.github.com>
2026-01-13 11:47:28 +0530

    Update run_fract.py

--- fractalbrain/run_fract.py
+++ fractalbrain/run_fract.py
@@ -1,1 +1,12 @@
+fractalbrain.fract import fract
 
+run_fract(subjid, image, output_folder=None)
+    if image_extension == '.nii' or image_extension == '.nii.gz':
+        print ("The prefix is: ", subjid)
+        print ("The NifTI image is: ", image)
+        if output_folder is None:
+          print ("The output_folder is the current directory")
+        else:
+          print ("The output_folder is: ", output_folder)
+      
+        fract(subjid, image, output_folder)
