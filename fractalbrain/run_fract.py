c762d9ed28389cc1a77be8b28673a40c9849c76d
rohan3412 <114685156+rohan3412@users.noreply.github.com>
2026-01-14 11:21:42 +0530

    Update run_fract.py

--- fractalbrain/run_fract.py
+++ fractalbrain/run_fract.py
@@ -1,18 +1,16 @@
 from .fract import fract
 import os
+import nibabel as nib
 
 def run_fract(subjid, image, output_folder=None):
-    imagefile = os.path.basename(image)
-    imagename, image_extension1 = os.path.splitext(imagefile)
-    imagename, image_extension2 = os.path.splitext(imagename)
-    image_extension = image_extension2 + image_extension1
-
-    if image_extension == '.nii' or image_extension == '.nii.gz':
-        print ("The prefix is: ", subjid)
-        print ("The NifTI image is: ", image)
-        if output_folder is None:
-          print ("The output_folder is the current directory")
-        else:
-          print ("The output_folder is: ", output_folder)
-      
+    if output_folder is None:
+        print ("The output_folder is the current directory")
+    else:
+        print ("The output_folder is: ", output_folder)
+        
+    if isinstance(image, nib.Nifti1Image):
         fract(subjid, image, output_folder)
+    elif image_extension == '.nii' or image_extension == '.nii.gz':
+        fract(subjid, image, output_folder)
+    else:
+        print("ERROR: The passed image should be nifti file or nifti file path")
