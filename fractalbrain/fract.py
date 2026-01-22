30b912f8c23763e1f00ccb8f28048af5ac124d9b
rohan3412 <114685156+rohan3412@users.noreply.github.com>
2026-01-22 13:26:29 +0530

    Update fract.py

--- fractalbrain/fract.py
+++ fractalbrain/fract.py
@@ -2,8 +2,8 @@ from fractalbrain.asofi import asofi
 
 def fract(subjid, image, output_folder=None,scaling_method="exponential"):
     print(f"Starting fract for {subjid}\n")
-    asofi(subjid, image, output_folder,scaling_method)
-    return;
+    return asofi(subjid, image, output_folder,scaling_method)
+    
             
     
     
