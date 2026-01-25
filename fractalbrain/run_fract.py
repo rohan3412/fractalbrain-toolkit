1ca5fbc3da5a1f25271be48a6742086e26e45e5f
rohan3412 <114685156+rohan3412@users.noreply.github.com>
2026-01-25 12:30:21 +0530

    Add save_plots parameter to run_fract function

--- fractalbrain/run_fract.py
+++ fractalbrain/run_fract.py
@@ -2,7 +2,7 @@ from .fract import fract
 import os
 import nibabel as nib
 
-def run_fract(subjid, image, output_folder=None, scaling_method="exponential"):
+def run_fract(subjid, image, output_folder=None, scaling_method="exponential", save_plots=True):
     if output_folder is None:
         print ("The output_folder is the current directory")
         output_folder = os.getcwd()
@@ -10,8 +10,8 @@ def run_fract(subjid, image, output_folder=None, scaling_method="exponential"):
         print ("The output_folder is: ", output_folder)
         
     if isinstance(image, nib.Nifti1Image):
-        return fract(subjid, image, output_folder, scaling_method)
+        return fract(subjid, image, output_folder, scaling_method, save_plots)
     elif isinstance(image, str) and (image.endswith('.nii') or image.endswith('.nii.gz')):
-        return fract(subjid, image, output_folder, scaling_method)
+        return fract(subjid, image, output_folder, scaling_method, save_plots)
     else:
         print("ERROR: The passed image should be nifti file or nifti file path")
